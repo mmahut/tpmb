7455763a36b128cb1d9a7acab3cec51ea7d53b97
Marek Mahut <marek.mahut@satoshilabs.com>
2019-08-29 10:38:52 +0200

    arduino: adding double press

--- arduino/tpmb.ino
+++ arduino/tpmb.ino
@@ -29,40 +29,43 @@ void loop() {
     if(Serial.available()){
         command = Serial.readStringUntil('\n');
         
-        // Press trezor's right button
         if(command.equals("right press")){
           Serial.println("Pressing right button.");
           servo_right.write(right_pressed);
         }
-        // Unpress trezor's right button
         else if(command.equals("right unpress")){
           Serial.println("Unpressing right button.");
           servo_right.write(right_unpressed);
         }
-        // Press trezor's left button
         else if(command.equals("left press")){
         Serial.println("Pressing the left button.");
         servo_left.write(left_pressed);
         }
-        // Unpress trezor's left button
         else if(command.equals("left unpress")){
           Serial.println("Unpressing the left button.");
           servo_left.write(left_unpressed);
         }
-        // click trezor's left button
         else if(command.equals("left click")){
           Serial.println("Clicking the left button.");
           servo_left.write(left_pressed);
           delay(500);
           servo_left.write(left_unpressed);
         }
-        // click trezor's right button
         else if(command.equals("right click")){
           Serial.println("Clicking the right button.");
           servo_right.write(right_pressed);
           delay(500);
           servo_right.write(right_unpressed);
-
+        }
+        else if(command.equals("all press")){
+          Serial.println("Pressing all buttons.");
+          servo_right.write(right_pressed);
+					servo_left.write(left_pressed);
+        }
+        else if(command.equals("all unpress")){
+          Serial.println("Unpressing all buttons.");
+          servo_right.write(right_unpressed);
+          servo_left.write(left_unpressed);
         }
     }
 }
