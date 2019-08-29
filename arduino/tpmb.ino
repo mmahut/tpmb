5e636946d924110badaacf841294830c6666fbcc
Marek Mahut <marek.mahut@satoshilabs.com>
2019-08-29 08:35:57 +0200

    arduino: adding click

--- arduino/tpmb.ino
+++ arduino/tpmb.ino
@@ -1,8 +1,11 @@
 
 #include <Servo.h>
 
-#define pressed 90
-#define unpressed 0
+#define right_unpressed 60
+#define right_pressed 0
+#define left_unpressed 0
+#define left_pressed 60
+
 
 String command;
 Servo servo_right;
@@ -16,9 +19,9 @@ void setup() {
     servo_right.attach(7);
     servo_left.attach(8);
     
-    // Setting them in position zero
-    servo_right.write(0);
-    servo_left.write(0);
+    // Setting them un unpressed position
+    servo_right.write(right_unpressed);
+    servo_left.write(left_unpressed);
     Serial.println("Ready for commands, press my buttons!");
 }
  
@@ -29,25 +32,37 @@ void loop() {
         // Press trezor's right button
         if(command.equals("right press")){
           Serial.println("Pressing right button.");
-          servo_right.write(pressed);
+          servo_right.write(right_pressed);
         }
-        
         // Unpress trezor's right button
         else if(command.equals("right unpress")){
           Serial.println("Unpressing right button.");
-          servo_right.write(unpressed);
+          servo_right.write(right_unpressed);
         }
-        
         // Press trezor's left button
         else if(command.equals("left press")){
         Serial.println("Pressing the left button.");
-        servo_left.write(pressed);
+        servo_left.write(left_pressed);
         }
-        
         // Unpress trezor's left button
         else if(command.equals("left unpress")){
           Serial.println("Unpressing the left button.");
-          servo_right.write(unpressed);
+          servo_left.write(left_unpressed);
+        }
+        // click trezor's left button
+        else if(command.equals("left click")){
+          Serial.println("Clicking the left button.");
+          servo_left.write(left_pressed);
+          delay(500);
+          servo_left.write(left_unpressed);
+        }
+        // click trezor's right button
+        else if(command.equals("right click")){
+          Serial.println("Clicking the right button.");
+          servo_right.write(right_pressed);
+          delay(500);
+          servo_right.write(right_unpressed);
+
         }
     }
 }
