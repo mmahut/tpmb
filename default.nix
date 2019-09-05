f2e93f25d1a2b7c66bb93ef435483e38b6c45e36
Marek Mahut <marek.mahut@satoshilabs.com>
2019-09-05 15:26:36 +0200

    default.nix: pinning packages and adding yaml

--- default.nix
+++ default.nix
@@ -1,9 +1,12 @@
-with import <nixpkgs> {};
-
+with import (fetchTarball {
+  url = "https://github.com/nixos/nixpkgs-channels/archive/c4adeddb5f8e945517068968d06ea838b7c24bd3.tar.gz";
+  sha256 = "1vpm73y7d0j2cviq0cgjwdj64h2v0c349capiyqf5f6071anx7d7";
+}) { };
 let
   pythonModules = python37.withPackages(ps: [
     ps.pyserial
     ps.trezor
+    ps.pyyaml
   ]);
 in
 
